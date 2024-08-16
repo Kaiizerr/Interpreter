68a926644b86ea883bcb4522411d58f835382c17
Karn Babrah <132544522+Kaiizerr@users.noreply.github.com>
2024-08-16 14:58:29 +0100

    Add files via upload

---
+++ MainForm/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows.Forms;
+
+partial class Form1
+{
+    private System.ComponentModel.IContainer components = null;
+    private TextBox txtExpression;
+    private Button btnEvaluate;
+    private Label lblResult;
+    private PictureBox pictureBoxCanvas;
+    private TextBox txtSlope;
+    private TextBox txtIntercept;
+    private Button btnDrawLine;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        this.txtExpression = new System.Windows.Forms.TextBox();
+        this.btnEvaluate = new System.Windows.Forms.Button();
+        this.lblResult = new System.Windows.Forms.Label();
+        this.pictureBoxCanvas = new System.Windows.Forms.PictureBox();
+        this.txtSlope = new System.Windows.Forms.TextBox();
+        this.txtIntercept = new System.Windows.Forms.TextBox();
+        this.btnDrawLine = new System.Windows.Forms.Button();
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).BeginInit();
+        this.SuspendLayout();
+
+        // 
+        // txtExpression
+        // 
+        this.txtExpression.Location = new System.Drawing.Point(12, 12);
+        this.txtExpression.Name = "txtExpression";
+        this.txtExpression.Size = new System.Drawing.Size(200, 20);
+        this.txtExpression.TabIndex = 0;
+
+        // 
+        // btnEvaluate
+        // 
+        this.btnEvaluate.Location = new System.Drawing.Point(218, 10);
+        this.btnEvaluate.Name = "btnEvaluate";
+        this.btnEvaluate.Size = new System.Drawing.Size(75, 23);
+        this.btnEvaluate.TabIndex = 1;
+        this.btnEvaluate.Text = "Evaluate";
+        this.btnEvaluate.UseVisualStyleBackColor = true;
+        this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
+
+        // 
+        // lblResult
+        // 
+        this.lblResult.AutoSize = true;
+        this.lblResult.Location = new System.Drawing.Point(299, 15);
+        this.lblResult.Name = "lblResult";
+        this.lblResult.Size = new System.Drawing.Size(37, 13);
+        this.lblResult.TabIndex = 2;
+        this.lblResult.Text = "Result:";
+
+        // 
+        // pictureBoxCanvas
+        // 
+        this.pictureBoxCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.pictureBoxCanvas.Location = new System.Drawing.Point(12, 50);
+        this.pictureBoxCanvas.Name = "pictureBoxCanvas";
+        this.pictureBoxCanvas.Size = new System.Drawing.Size(400, 300);
+        this.pictureBoxCanvas.TabIndex = 3;
+        this.pictureBoxCanvas.TabStop = false;
+
+        // 
+        // txtSlope
+        // 
+        this.txtSlope.Location = new System.Drawing.Point(12, 360);
+        this.txtSlope.Name = "txtSlope";
+        this.txtSlope.Size = new System.Drawing.Size(100, 20);
+        this.txtSlope.TabIndex = 4;
+
+        // 
+        // txtIntercept
+        // 
+        this.txtIntercept.Location = new System.Drawing.Point(118, 360);
+        this.txtIntercept.Name = "txtIntercept";
+        this.txtIntercept.Size = new System.Drawing.Size(100, 20);
+        this.txtIntercept.TabIndex = 5;
+
+        // btnDrawLine
+        this.btnDrawLine.Location = new System.Drawing.Point(224, 358);
+        this.btnDrawLine.Name = "btnDrawLine";
+        this.btnDrawLine.Size = new System.Drawing.Size(75, 23);
+        this.btnDrawLine.TabIndex = 6;
+        this.btnDrawLine.Text = "Draw Line";
+        this.btnDrawLine.UseVisualStyleBackColor = true;
+        this.btnDrawLine.Click += new System.EventHandler(this.btnDrawLine_Click);
+
+        // Form1 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(424, 391);
+        this.Controls.Add(this.btnDrawLine);
+        this.Controls.Add(this.txtIntercept);
+        this.Controls.Add(this.txtSlope);
+        this.Controls.Add(this.pictureBoxCanvas);
+        this.Controls.Add(this.lblResult);
+        this.Controls.Add(this.btnEvaluate);
+        this.Controls.Add(this.txtExpression);
+        this.Name = "Form1";
+        this.Text = "Math Expression Evaluator";
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCanvas)).EndInit();
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+}
---
+++ MainForm/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.FSharp.Collections;
+using InterpreterLib;
+
+public partial class Form1 : Form
+{
+    private double slope;
+    private double intercept;
+    private List<Tuple<string, double>> symbolTable = new List<Tuple<string, double>>();
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void btnEvaluate_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string expression = txtExpression.Text;
+            var tokens = Interpreter.lexer(expression).ToList();
+
+            // Convert List<Interpreter.terminal> to FSharpList<Interpreter.terminal>
+            var fsharpTokens = ListModule.OfSeq(tokens);
+
+            // Convert List<Tuple<string, double>> to FSharpList<Tuple<string, double>>
+            var symbolTableFSharp = ListModule.OfSeq(symbolTable);
+
+            // Evaluate expression
+            var result = Interpreter.parseNeval(fsharpTokens, symbolTableFSharp);
+            lblResult.Text = $"Result: {result.Item2}";
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void btnDrawLine_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            slope = double.Parse(txtSlope.Text);
+            intercept = double.Parse(txtIntercept.Text);
+            DrawLine(slope, intercept);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Invalid input for slope or intercept.");
+        }
+    }
+
+    private void DrawLine(double slope, double intercept)
+    {
+        if (pictureBoxCanvas.Image == null)
+        {
+            pictureBoxCanvas.Image = new Bitmap(pictureBoxCanvas.Width, pictureBoxCanvas.Height);
+        }
+
+        using (Graphics g = Graphics.FromImage(pictureBoxCanvas.Image))
+        {
+            g.Clear(Color.White); // Clear previous drawings
+
+            Pen pen = new Pen(Color.Black, 2);
+
+            int width = pictureBoxCanvas.Width;
+            int height = pictureBoxCanvas.Height;
+
+            PointF startPoint = new PointF(0, (float)(slope * 0 + intercept));
+            PointF endPoint = new PointF(width, (float)(slope * width + intercept));
+
+            startPoint.Y = Clamp(startPoint.Y, 0, height);
+            endPoint.Y = Clamp(endPoint.Y, 0, height);
+
+            g.DrawLine(pen, startPoint, endPoint);
+
+            pictureBoxCanvas.Refresh();
+        }
+    }
+
+    private float Clamp(float value, float min, float max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+}
